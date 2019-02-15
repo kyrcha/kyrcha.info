f3dad15260558b1367159a4e70660b7bdee8d696
Kyriakos Chatzidimitriou <kyrcha@gmail.com>
2019-02-15 17:42:54 +0200

    Change text

--- src/pages/development/cenote.js
+++ src/pages/development/cenote.js
@@ -12,7 +12,8 @@ const Cenote = () => {
             <p>
                 <OutboundLink href='https://en.wikipedia.org/wiki/Cenote'>Cenote</OutboundLink> (Katavothra in Greek) 
                 is a Big Data Management System (BDMS) for event processing and analytics. Our goal was to build an 
-                open source equivalent of <OutboundLink href='http://keen.io'>keen.io</OutboundLink> analytics and learn about scalable systems in the process.
+                open source alternative to <OutboundLink href='http://keen.io'>keen.io</OutboundLink> analytics and 
+                learn about scalable systems in the process.
             </p>
             <h2>Tech Stack</h2>
             <span className="tag is-rounded is-primary">javascript</span><span> </span>
